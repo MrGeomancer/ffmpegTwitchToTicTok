0bc8799d62bf07355b4ee2a754ada82f587a9bc8
MrGeomancer <101983215+MrGeomancer@users.noreply.github.com>
2024-02-20 18:46:46 +0500

    начало tkinter

--- main.py
+++ main.py
@@ -1,37 +1,58 @@
-# import ffmpeg
-#
-# stream = ffmpeg.input(r'in.mp4')
-# # stream = ffmpeg.filter(scale='1080:1920', setsar='1:1')
-# stream = ffmpeg.filter(stream, 'scale', 'scale=1080:1920, setsar=1:1')
-# output_stream = ffmpeg.output(stream,r'out2.mp4')
-# ffmpeg.run(output_stream)
+from tkinter import *
+from tkinter import filedialog
+from tkinter import ttk
+from PIL import Image, ImageTk
 
-import ffmpeg
-
-# Входной видеофайл
-input_file = 'in.mp4'
-
-# Задаем входной поток
-stream = ffmpeg.input(input_file)
-
-# Фильтры для растягивания и установки соотношения сторон
-filter_options = 'scale=1080:1920,setsar=1:1'
-
-# Добавляем фильтр матового стекла
-frosted_glass_filter = 'gblur=sigma=3'
-
-# boxblur=luma_radius=min(h\,w)/40:luma_power=3:chroma_radius=min(cw\,ch)/40:chroma_power=1[bg]
-
-# Задаем выходной поток с использованием обоих фильтров
-output_stream = ffmpeg.output(stream, 'output_frosted_glass_video5.mp4', vf=f'{filter_options},{frosted_glass_filter}', aspect='9:16')
-ffmpeg.run(output_stream, capture_stderr=True, overwrite_output=True, capture_stdout=True)
+path = str
+photo = None  # Глобальная переменная для изображения
+video_cap = None  # Глобальная переменная для каптчера видео
 
 
+def resize_photo(image,canvas):
+    img_width, img_height = image.size
+    canvas_width, canvas_height = canvas.winfo_reqwidth(), canvas.winfo_reqheight()
+    scale_x = canvas_width / img_width
+    scale_y = canvas_height / img_height
+    scale_factor = min(scale_x, scale_y)
+    image = image.resize((int(img_width * scale_factor), int(img_height * scale_factor)))
+    return ImageTk.PhotoImage(image)
 
-# Запускаем команду
-try:
-    ffmpeg.run(output_stream, capture_stderr=True, overwrite_output=True, capture_stdout=True)
-except ffmpeg.Error as e:
-    print(e.stderr.decode())
+
+def explore():
+    global path
+    path = filedialog.askopenfilename()
+    lbl2.configure(text=path)
+    # lbl2.configure(width=len(path))
+
+def prewatch(path):
+    global photo
+    canvas = Canvas(window, width=1080, height=720)
+    canvas.grid(column=3, row=2)
+    canvas.delete("all")
+
+    image = Image.open(path)
+    photo = resize_photo(image,canvas)
+
+    Label(window, text=path).grid(column=3, row=1)
+    canvas.create_image(0, 0, anchor=NW, image=photo)
 
 
+window = Tk()
+window.title("Добро пожаловать в приложение PythonRu")
+
+lbl = Label(window, text="Введите путь к на видео:", font=("Arial Bold", 12))
+lbl.grid(column=0, row=0)
+
+lbl2 = Label(window, text=f"", font=("Arial Bold", 12), bg='lightgray', width=50)
+lbl2.grid(column=0, row=1)
+
+btn = ttk.Button(window, text="Обзор..", command=explore)
+btn.grid(column=1, row=1)
+
+btn2 = Button(window, text="Предпросмотр", command=lambda: prewatch(path))
+btn2.grid(column=1, row=2)
+
+
+
+window.geometry('1800x900')
+window.mainloop()
