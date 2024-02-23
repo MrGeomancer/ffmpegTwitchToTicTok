8e9d2f90c78cb1dd83041bb32dfb3996124502ff
MrGeomancer <101983215+MrGeomancer@users.noreply.github.com>
2024-02-23 18:02:20 +0500

    Update gigachad2.py

--- gigachad2.py
+++ gigachad2.py
@@ -1,12 +1,14 @@
 import ffmpeg
+import os
 
 
-def render_blur(path):
+def render_blur(path, outputs):
     scalefilt = f'scale={rez['width']}:{rez['length']},setsar=1:1'
     blurfilt = 'boxblur=luma_radius=27'
     blurfilt2 = 'boxblur=luma_power=3'
     blurfilt3 = 'boxblur=chroma_radius=27'
     blurfilt4 = 'boxblur=luma_power=3'
+    
     making_bg = (
         ffmpeg
         .input(path)
@@ -17,7 +19,9 @@ def render_blur(path):
 
 def render(outputs, path, **kwargs):
     if kwargs['cs_to916']:
-        render_blur(path,outputs)
+        if kwargs['cs_crop']:
+            render_blur(path,outputs, 1)
+        else: render_blur(path,outputs, 0)
     for i in kwargs:
         print(kwargs)
     print(outputs)
