fb6b469e516d0309701b0de2a3ba67ff26575fce
MrGeomancer <101983215+MrGeomancer@users.noreply.github.com>
2024-03-04 19:27:53 +0500

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,25 @@
+@echo off
+
+echo Активация виртуального окружения...
+call .venv\Scripts\activate
+if errorlevel 1 (
+    echo Ошибка при активации виртуального окружения.
+    echo Нажмите любую клавишу, чтобы закрыть окно...
+    pause
+    exit /b 1
+)
+
+echo Запуск main.py...
+python main.py
+if errorlevel 1 (
+    echo Ошибка при выполнении main.py.
+    echo Нажмите любую клавишу, чтобы закрыть окно...
+    pause
+    exit /b 1
+)
+
+echo Завершение работы виртуального окружения...
+deactivate
+
+echo Нажмите любую клавишу, чтобы закрыть окно...
+pause
