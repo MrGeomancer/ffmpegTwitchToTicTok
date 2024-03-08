dab914f93e36447dde05481d658e82fb9b8b8391
MrGeomancer <101983215+MrGeomancer@users.noreply.github.com>
2024-03-08 18:22:41 +0500

    Update gigachad2.py

--- gigachad2.py
+++ gigachad2.py
@@ -6,11 +6,11 @@ def render_crop(path, scale, rez_orig):
     # print('scale:',scale)
     # type(scale)
     global rez
-    crop_x = rez_orig['width']*scale
+    crop_x = rez_orig['width'] * scale
     crop_y = rez_orig['height']
-    crop_from_x = rez_orig['width']/2-crop_x/2
-    scale_to_y = int(rez['width']/crop_x * crop_y)
-    overlay_y = int(rez['height']/2 - scale_to_y/2)
+    crop_from_x = rez_orig['width'] / 2 - crop_x / 2
+    scale_to_y = int(rez['width'] / crop_x * crop_y)
+    overlay_y = int(rez['height'] / 2 - scale_to_y / 2)
     croped_path = fr'{path}_folder/{scale}x{int(crop_x)}x{int(crop_y)}croped.mp4'
     print('overlay_y:', overlay_y)
     print('rez[widthg]', rez['width'])
@@ -43,6 +43,7 @@ def render_crop(path, scale, rez_orig):
     )
     return croped_path
 
+
 def render_kills_cs(path, rez_orig, scale):
     global rez
     crop_x = 285
@@ -50,7 +51,8 @@ def render_kills_cs(path, rez_orig, scale):
     scale_to_x = crop_x * 1.5
     scale_to_y = crop_y * 1.5
     overlay_x = int(rez['width'] - scale_to_x)
-    overlay_y = ((rez['height']/2)-(((rez['width']/(rez_orig['width']*scale)) * rez_orig['height'])/2))-scale_to_y
+    overlay_y = ((rez['height'] / 2) - (
+                ((rez['width'] / (rez_orig['width'] * scale)) * rez_orig['height']) / 2)) - scale_to_y
     #
     # print('высота кадра:',rez['height'])
     # print('высота оригинального видоса:',int((rez['width']/(rez_orig['width']*scale)) * rez_orig['height']))
@@ -93,7 +95,8 @@ def render_radar_cs(path, rez_orig, scale):
     crop_y = 289
     scale_to_x = crop_x * 1.5
     scale_to_y = crop_y * 1.5
-    overlay_y = ((rez['height']/2)-(((rez['width']/(rez_orig['width']*scale)) * rez_orig['height'])/2))-scale_to_y
+    overlay_y = ((rez['height'] / 2) - (
+                ((rez['width'] / (rez_orig['width'] * scale)) * rez_orig['height']) / 2)) - scale_to_y
 
     making_radar = (
         ffmpeg
@@ -101,7 +104,7 @@ def render_radar_cs(path, rez_orig, scale):
         .overlay(
             ffmpeg
             .input(path)
-            .filter('crop', 289,289,6,6)
+            .filter('crop', 289, 289, 6, 6)
             .filter('scale', scale_to_x, scale_to_y)
             , x=0
             , y=overlay_y
@@ -109,7 +112,9 @@ def render_radar_cs(path, rez_orig, scale):
         .output(fr'{path}_folder/radar_on_bg.mp4', vcodec='libx264')
         .run(overwrite_output=False)
     )
- # making_radar = (
+
+
+# making_radar = (
 #     ffmpeg
 #     .input(input_video)
 #     .filter('crop', 289,289,6,6)
@@ -118,38 +123,57 @@ def render_radar_cs(path, rez_orig, scale):
 # )
 
 
-def render_mask_apex(path, rez_orig, scale):
+def render_mask_apex(path, armor):
     global rez
-
-    making_apex_mask = (
-        ffmpeg
-        .input(path)
-        .overlay(
+    if armor == True:
+        making_apex_mask = (
             ffmpeg
-            .input(r'stock/mask-apex.png')
-            , x=0
-            , y=0
+            .input(path)
+            .overlay(
+                ffmpeg
+                .input(r'stock/mask-apex-with-armor.png')
+                , x=0
+                , y=0
+            )
+            .output(fr'{path}_folder/apex_mask.mp4', vcodec='libx264')
+            .run(overwrite_output=False)
         )
-        .output(fr'{path}_folder/apex_mask.mp4', vcodec='libx264')
-        .run(overwrite_output=False)
-    )
+    else:
+        making_apex_mask = (
+            ffmpeg
+            .input(path)
+            .overlay(
+                ffmpeg
+                .input(r'stock/mask-apex.png')
+                , x=0
+                , y=0
+            )
+            .output(fr'{path}_folder/apex_mask.mp4', vcodec='libx264')
+            .run(overwrite_output=False)
+        )
 
 
-def render_radar_apex(path, rez_orig, scale):
+def render_radar_apex(path, rez_orig, scale, kwargs):
     global rez
     crop_x = 244
     crop_y = 244
     scale_to_x = crop_x * 1.5
     scale_to_y = crop_y * 1.5
-    overlay_y = ((rez['height']/2)-(((rez['width']/(rez_orig['width']*scale)) * rez_orig['height'])/2))-scale_to_y
-
+    overlay_y = ((rez['height'] / 2) - (
+                ((rez['width'] / (rez_orig['width'] * scale)) * rez_orig['height']) / 2)) - scale_to_y
+    if kwargs['apex_hp'] == True:
+        overlay_y -= 178
+    elif kwargs['apex_hp_armor'] == True:
+        overlay_y -= 220
+    if kwargs['apex_zone'] == True:
+        overlay_y -= 81
     making_radar = (
         ffmpeg
         .input(fr'{path}_folder/bg.mp4')
         .overlay(
             ffmpeg
             .input(fr'{path}_folder/apex_mask.mp4')
-            .filter('crop', 244,244,48,48)
+            .filter('crop', 244, 244, 48, 48)
             .filter('scale', scale_to_x, scale_to_y)
             , x=2
             , y=overlay_y
@@ -159,13 +183,43 @@ def render_radar_apex(path, rez_orig, scale):
     )
 
 
+def render_zone_apex(path, rez_orig, scale, kwargs):
+    global rez
+
+    crop_x = 244
+    crop_y = 53
+    scale_to_x = crop_x * 1.5
+    scale_to_y = crop_y * 1.5
+    overlay_y = ((rez['height'] / 2) - (
+                ((rez['width'] / (rez_orig['width'] * scale)) * rez_orig['height']) / 2)) - scale_to_y
+    if kwargs['apex_hp'] == True:
+        overlay_y -= 179
+    elif kwargs['apex_hp_armor'] == True:
+        overlay_y -= 220
+    making_radar = (
+        ffmpeg
+        .input(fr'{path}_folder/bg.mp4')
+        .overlay(
+            ffmpeg
+            .input(fr'{path}_folder/apex_mask.mp4')
+            .filter('crop', crop_x, crop_y, 45, 305)
+            .filter('scale', scale_to_x, scale_to_y)
+            , x=2
+            , y=overlay_y
+        )
+        .output(fr'{path}_folder/zone_on_bg.mp4', vcodec='libx264')
+        .run(overwrite_output=False)
+    )
+
+
 def render_hp_apex(path, rez_orig, scale):
     global rez
     crop_x = 380
     crop_y = 71
-    scale_to_x = crop_x * 1.5
-    scale_to_y = crop_y * 1.5
-    overlay_y = ((rez['height']/2)-(((rez['width']/(rez_orig['width']*scale)) * rez_orig['height'])/2))-scale_to_y
+    scale_to_x = crop_x * 2.5
+    scale_to_y = crop_y * 2.5
+    overlay_y = ((rez['height'] / 2) - (
+                ((rez['width'] / (rez_orig['width'] * scale)) * rez_orig['height']) / 2)) - scale_to_y
 
     making_radar = (
         ffmpeg
@@ -173,7 +227,7 @@ def render_hp_apex(path, rez_orig, scale):
         .overlay(
             ffmpeg
             .input(fr'{path}_folder/apex_mask.mp4')
-            .filter('crop', 380,71,57,956)
+            .filter('crop', 380, 71, 57, 956)
             .filter('scale', scale_to_x, scale_to_y)
             , x=2
             , y=overlay_y
@@ -183,14 +237,80 @@ def render_hp_apex(path, rez_orig, scale):
     )
 
 
+def render_hp_armor_apex(path, rez_orig, scale):
+    global rez
+    crop_x = 423
+    crop_y = 110
+    scale_to_x = crop_x * 2
+    scale_to_y = crop_y * 2
+    overlay_y = ((rez['height'] / 2) - (
+                ((rez['width'] / (rez_orig['width'] * scale)) * rez_orig['height']) / 2)) - scale_to_y
+
+    making_radar = (
+        ffmpeg
+        .input(fr'{path}_folder/bg.mp4')
+        .overlay(
+            ffmpeg
+            .input(fr'{path}_folder/apex_mask.mp4')
+            .filter('crop', 423, 110, 13, 917)
+            .filter('scale', scale_to_x, scale_to_y)
+            , x=2
+            , y=overlay_y
+        )
+        .output(fr'{path}_folder/hp_on_bg.mp4', vcodec='libx264')
+        .run(overwrite_output=False)
+    )
+
+
+def render_kills_apex(path, rez_orig, scale):
+    global rez
+    crop_x = 207
+    crop_y = 32
+    scale_to_x1 = crop_x * 1.9
+    scale_to_y1 = crop_y * 1.9
+    overlay_x1 = rez['width'] - scale_to_x1
+    overlay_y1 = ((rez['height'] / 2) - (
+                ((rez['width'] / (rez_orig['width'] * scale)) * rez_orig['height']) / 2)) - scale_to_y1
+    scale_to_x2 = 103 * 1.9
+    scale_to_y2 = 32 * 1.9
+    overlay_x2 = rez['width'] - scale_to_x1 - scale_to_x2
+    overlay_y2 = ((rez['height'] / 2) - (
+            ((rez['width'] / (rez_orig['width'] * scale)) * rez_orig['height']) / 2)) - scale_to_y1
+
+
+    making_kills = (
+        ffmpeg
+        .input(fr'{path}_folder/bg.mp4')
+        .overlay(
+            ffmpeg
+            .input(fr'{path}_folder/apex_mask.mp4')
+            .filter('crop', crop_x, crop_y, 1442, 92)
+            .filter('scale', scale_to_x1, scale_to_y1)
+            , x=overlay_x1
+            , y=overlay_y1
+        )
+        .overlay(
+            ffmpeg
+            .input(fr'{path}_folder/apex_mask.mp4')
+            .filter('crop', 103, 32, 1662, 92)
+            .filter('scale', scale_to_x2, scale_to_y2)
+            , x=overlay_x2
+            , y=overlay_y2
+        )
+        .output(fr'{path}_folder/kills_on_bg.mp4', vcodec='libx264')
+        .run(overwrite_output=False)
+    )
+
+
 def render_players_cs(path, rez_orig, scale):
     global rez
     crop_x = 680
     crop_y = 180
-    scale_to_x =crop_x*1.5
-    scale_to_y =crop_y*1.5
-    overlay_x = int(rez['width']/2 - scale_to_x/2)
-    overlay_y = ((rez['height']/2)-(((rez['width']/(rez_orig['width']*scale)) * rez_orig['height'])/2))-scale_to_y
+    scale_to_x = crop_x * 1.5
+    scale_to_y = crop_y * 1.5
+    overlay_x = int(rez['width'] / 2 - scale_to_x / 2)
+    overlay_y = ((rez['height'] / 2) - (
+                ((rez['width'] / (rez_orig['width'] * scale)) * rez_orig['height']) / 2)) - scale_to_y
 
     making_players = (
         ffmpeg
@@ -198,7 +318,7 @@ def render_players_cs(path, rez_orig, scale):
         .overlay(
             ffmpeg
             .input(path)
-            .filter('crop', 680,180,620,0)
+            .filter('crop', 680, 180, 620, 0)
             .filter('scale', scale_to_x, scale_to_y)
             , x=overlay_x
             , y=overlay_y
@@ -206,6 +326,8 @@ def render_players_cs(path, rez_orig, scale):
         .output(fr'{path}_folder/players_on_bg.mp4', vcodec='libx264')
         .run(overwrite_output=False)
     )
+
+
 # making_players = (
 #     ffmpeg
 #     .input(input_video)
@@ -219,13 +341,13 @@ def render_webcam(path, rez_orig, scale):
     global rez
     crop_x = 270
     crop_y = 208
-    scale_to_x =crop_x*1.8
-    scale_to_y =crop_y*1.8
+    scale_to_x = crop_x * 1.8
+    scale_to_y = crop_y * 1.8
     if scale == 1:
-        scale_to_x*=1.5
-        scale_to_y*=1.5
-    overlay_x = int(rez['width']/2 - scale_to_x/2)
-    overlay_y = ((rez['height']/2)+(((rez['width']/(rez_orig['width']*scale)) * rez_orig['height'])/2))
+        scale_to_x *= 1.5
+        scale_to_y *= 1.5
+    overlay_x = int(rez['width'] / 2 - scale_to_x / 2)
+    overlay_y = ((rez['height'] / 2) + (((rez['width'] / (rez_orig['width'] * scale)) * rez_orig['height']) / 2))
 
     # print('scale_to_x:',scale_to_x)
     # print('scale_to_y:',scale_to_y)
@@ -235,7 +357,7 @@ def render_webcam(path, rez_orig, scale):
         .overlay(
             ffmpeg
             .input(path)
-            .filter('crop', crop_x,crop_y,37,381)
+            .filter('crop', crop_x, crop_y, 37, 381)
             .filter('scale', scale_to_x, scale_to_y)
             , x=overlay_x
             , y=overlay_y
@@ -256,29 +378,33 @@ def render_blur(path, blurcroped, croped_path=None):
         making_bg = (
             ffmpeg
             .input(croped_path)
-            .output(fr'{path}_folder/blured.mp4', vcodec='libx264', vf=f'{scalefilt}, {blurfilt},{blurfilt2}, {blurfilt3},{blurfilt4}')
+            .output(fr'{path}_folder/blured.mp4', vcodec='libx264',
+                    vf=f'{scalefilt}, {blurfilt},{blurfilt2}, {blurfilt3},{blurfilt4}')
             .run()
         )
     else:
         making_bg = (
             ffmpeg
             .input(path)
-            .output(fr'{path}_folder/blured.mp4', vcodec='libx264', vf=f'{scalefilt}, {blurfilt},{blurfilt2}, {blurfilt3},{blurfilt4}')
+            .output(fr'{path}_folder/blured.mp4', vcodec='libx264',
+                    vf=f'{scalefilt}, {blurfilt},{blurfilt2}, {blurfilt3},{blurfilt4}')
             .run()
         )
 
 
 def overlayall(path, outputs):
-    inputs=[ffmpeg.input(path) for path in outputs]
+    inputs = [ffmpeg.input(path) for path in outputs]
     global rez
     overlay = overlay = ffmpeg.input(fr'{path}_folder/blured.mp4')
     for item in outputs:
-        overlay=overlay.overlay(ffmpeg.input(item).filter('chromakey', color='0x00FF00', similarity=0.2, blend=0.1),x=0,y=0)
-    overlay = overlay.overlay(ffmpeg.input(r'stock/out4.mp4').filter('trim', duration=f"{ffmpeg.probe(path)['streams'][0]['duration']}"), x='342', y=400)  # add nameing
+        overlay = overlay.overlay(ffmpeg.input(item).filter('chromakey', color='0x00FF00', similarity=0.2, blend=0.2),
+                                  x=0, y=0)
+
+    overlay = overlay.overlay(
+        ffmpeg.input(r'stock/out4.mp4').filter('trim', duration=f"{ffmpeg.probe(path)['streams'][0]['duration']}"),
+        x='342', y=400)  # add nameing
     overlay = overlay.output(ffmpeg.input(path).audio, fr'{path}_folder/final.mp4', vcodec='libx264', acodec='aac')
     ffmpeg.run(overlay, overwrite_output=True)
-
-
 
     # overlay_all = (
     #     ffmpeg
@@ -309,25 +435,25 @@ def overlayall(path, outputs):
 
 def take_rez(path):
     return {
-        'width':ffmpeg.probe(path)['streams'][0]['width'],
-        'height':ffmpeg.probe(path)['streams'][0]['height']
+        'width': ffmpeg.probe(path)['streams'][0]['width'],
+        'height': ffmpeg.probe(path)['streams'][0]['height']
     }
 
 
 def render(outputs, path, **kwargs):
     global rez
-    colvo = len(outputs)-1
+    colvo = len(outputs) - 1
     print('outputs:', outputs)
     os.makedirs(fr'{path}_folder/', exist_ok=True)
     rez_input = take_rez(path)
     making_bg = (
         ffmpeg
         .input(r'stock\stock_bg.png'
-               ,loop=1
-               ,t=1
+               , loop=1
+               , t=1
                )
         # .filter('scale', rez['width'], rez['height'])
-        .output(fr'{path}_folder/bg.mp4',vf=f'scale={rez['width']}:{rez['height']},setsar=1:1,fps=60')
+        .output(fr'{path}_folder/bg.mp4', vf=f'scale={rez['width']}:{rez['height']},setsar=1:1,fps=60')
         .run(overwrite_output=True)
     )
     try:
@@ -342,7 +468,8 @@ def render(outputs, path, **kwargs):
         if kwargs['any_crop'] == True:
             print('Начало рендера блюр из crop')
             render_blur(path, True, croped_path)
-        else: render_blur(path, False)
+        else:
+            render_blur(path, False)
     if kwargs['cs_kills'] == True:
         print('Начало рендера kills')
         render_kills_cs(path, rez_input, entry)
@@ -357,25 +484,36 @@ def render(outputs, path, **kwargs):
         render_webcam(path, rez_input, entry)
     if kwargs['apex_make_bg'] == True:
         print('Начало рендера apex_mask')
-        render_mask_apex(path, rez_input, entry)
+        if kwargs['apex_hp_armor'] == True:
+            render_mask_apex(path, armor=True)
+        else:
+            render_mask_apex(path, armor=False)
+    if kwargs['apex_zone'] == True:
+        print('Начало рендера apex_zone')
+        render_zone_apex(path, rez_input, entry, kwargs)
     if kwargs['apex_radar'] == True:
         print('Начало рендера apex_radar')
-        render_radar_apex(path, rez_input, entry)
+        render_radar_apex(path, rez_input, entry, kwargs)
     if kwargs['apex_hp'] == True:
         print('Начало рендера apex_hp')
         render_hp_apex(path, rez_input, entry)
+    if kwargs['apex_hp_armor'] == True:
+        print('Начало рендера apex_hp')
+        render_hp_armor_apex(path, rez_input, entry)
+    if kwargs['apex_kills'] == True:
+        print('Начало рендера apex_hp')
+        render_kills_apex(path, rez_input, entry)
 
-
-
-    overlayall(path,outputs)
+    overlayall(path, outputs)
     # for i in kwargs:
     #     print(kwargs)
     print(outputs)
 
 
 rez = {
-    'width':1440,
-    'height':2808,
+    'width': 1440,
+    'height': 2808,
     # 'chonit':'smth',
 }
 
+overlay_y_min = 400
